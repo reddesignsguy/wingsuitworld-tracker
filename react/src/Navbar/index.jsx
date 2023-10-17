5f5c390b32c42c8e65f8edcfa7a45fa368d94752
Albany Patriawan <zekromfx1337@gmail.com>
2023-10-17 15:45:59 -0700

    navbar, routing

---
+++ react/src/Navbar/index.jsx
@@ -0,0 +1,4 @@
+export { default } from './Navbar.jsx';            
+
+// Index.jsx file the default export for this folder
+// Separate index.jsx and Navbar.jsx because multiple indexes will exist and might make it confusing to read while coding
