1e41f686f2023458da53f85e364e62a044b836f9
Albany Patriawan <zekromfx1337@gmail.com>
2023-12-01 12:44:51 -0800

    Null edge case in SQL query builder

--- server/helpers/queryBuilders.js
+++ server/helpers/queryBuilders.js
@@ -1,13 +1,20 @@
 exports.buildUserPatchQuery = function (user) {
+    
     // User still retains all attributes, however, only patch non-null ones provided. Also, userId is what we search for, not update, which is why it's ignored
-    const updateFields = Object.keys(user).filter(key => user[key] != null && key != 'userId');
+    const updateFields = Object.keys(user).filter(key => user[key] !== undefined && key != 'userId');
 
     if (updateFields.length === 0) {
         // If there are no valid fields to update, return an empty string
         return '';
     }
-    const setClause = updateFields.map(field => `${field} = '${user[field]}'`).join(', ');
+
+    // 1) Converts {col1: val1, col2: val2} into 'col1 = val1, col2 = val2'
+    // 2) Null related code takes care of an SQL edge case and converts 'NULL' TO NULL
+    const setClause = updateFields.map(field => 
+        user[field] !== null ? `${field} = '${user[field]}'` : `${field} = NULL`)
+        .join(', ');
 
     const query = `UPDATE users SET ${setClause} WHERE userId = '${user.userId}'`;
+
     return query;
 }
