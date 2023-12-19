c36ca82e582a5fdad7315837236a8dd191ed7410
Albany Patriawan <zekromfx1337@gmail.com>
2023-12-18 20:11:50 -0800

    refactored navbar and increased readability

--- react/src/Navbar/Navbar.jsx
+++ react/src/Navbar/Navbar.jsx
@@ -31,36 +31,6 @@ export default function Navbar() {
     document.addEventListener("mousedown", handler);
   });
 
-  function LoggedInComponent(props) {
-    const { loginWithRedirect } = useAuth0();
-    let component;
-    // Show logged in state
-    if (props.loggedIn) {
-      component = (
-        <li className="picture_container" ref={picRef}>
-          <img
-            src="https://tr.rbxcdn.com/15DAY-Avatar-BC8E5946272E2104B2C0935DEA4D020D-Png/352/352/Avatar/Png/noFilter"
-            alt="Character image"
-            onClick={() => {
-              setOpen(!open);
-            }}
-          ></img>
-        </li>
-      );
-    }
-    // Show Login button
-    else {
-      component = (
-        <li>
-          <a className="inactive" onClick={() => loginWithRedirect()}>
-            Login
-          </a>
-        </li>
-      );
-    }
-    return component;
-  }
-
   return !isLoading ? (
     <>
       <link href="https://css.gg/css" rel="stylesheet" />{" "}
@@ -96,34 +66,79 @@ export default function Navbar() {
             </NavLink>
           </li>
           {/* TODO: Modify hard-coded picture */}
-          <LoggedInComponent loggedIn={isAuthenticated} />
+          <LoggedInComponent
+            loggedIn={isAuthenticated}
+            picRef={picRef}
+            open={open}
+            setOpen={setOpen}
+          />
         </ul>
       </nav>
-      <section
-        className={`dropdown-menu ${open ? "active" : "inactive"}`}
-        ref={menuRef}
-      >
-        <section className="info">
-          <h2> Vexeo</h2>
-          <span> Rank #3</span>
-        </section>
-        {/* Spans are divs but are in-line rather than block, used for marking up text */}
-        <hr />
-        <ul>
-          <DropdownItem
-            icon={<IoSettingsOutline size="1.3rem" />}
-            text="Settings"
-            link="/settings"
-          ></DropdownItem>
-          <DropdownItem
-            icon={<IoLogOut size="1.3rem" />}
-            text="Logout"
-            onClick={() => logout()}
-          ></DropdownItem>
-        </ul>
-      </section>
+      {/* Drop Down Menu */}
+      <DropDownMenu menuRef={menuRef} logout={logout} open={open} />
     </>
   ) : null;
+}
+
+// Decides if login button or profile picture is shown
+function LoggedInComponent(props) {
+  const { picRef, open, setOpen } = props;
+  const { loginWithRedirect } = useAuth0();
+  let component;
+  // Show logged in state
+  if (props.loggedIn) {
+    component = (
+      <li className="picture_container" ref={picRef}>
+        <img
+          src="https://tr.rbxcdn.com/15DAY-Avatar-BC8E5946272E2104B2C0935DEA4D020D-Png/352/352/Avatar/Png/noFilter"
+          alt="Character image"
+          onClick={() => {
+            setOpen(!open);
+          }}
+        ></img>
+      </li>
+    );
+  }
+  // Show Login button
+  else {
+    component = (
+      <li>
+        <a className="inactive" onClick={() => loginWithRedirect()}>
+          Login
+        </a>
+      </li>
+    );
+  }
+  return component;
+}
+
+function DropDownMenu(props) {
+  const { menuRef, logout, open } = props;
+  return (
+    <section
+      className={`dropdown-menu ${open ? "active" : "inactive"}`}
+      ref={menuRef}
+    >
+      <section className="info">
+        <h2> Vexeo</h2>
+        <span> Rank #3</span>
+      </section>
+      {/* Spans are divs but are in-line rather than block, used for marking up text */}
+      <hr />
+      <ul>
+        <DropdownItem
+          icon={<IoSettingsOutline size="1.3rem" />}
+          text="Settings"
+          link="/settings"
+        ></DropdownItem>
+        <DropdownItem
+          icon={<IoLogOut size="1.3rem" />}
+          text="Logout"
+          onClick={() => logout()}
+        ></DropdownItem>
+      </ul>
+    </section>
+  );
 }
 
 function DropdownItem(props) {
