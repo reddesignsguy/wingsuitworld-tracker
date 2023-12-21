a171f40e34f6102f570f407c35ca8e484cc0062b
Albany Patriawan <zekromfx1337@gmail.com>
2023-12-21 12:23:15 -0800

    decouple Apis from UI

--- react/src/apis/apis.js
+++ react/src/apis/apis.js
@@ -1,54 +1,70 @@
 // api.com/profile
 exports.claimProfile = async function (userId, playerName, profileCode) {
-    const data = {
-      userId: userId,
-      playerName: playerName,
-      profileCode: profileCode,
-    };
-  
-    // TODO Refactor and call from apis
-    var res = await fetch(`http://localhost:5051/profile/claim`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-    if (res.ok) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+  const data = {
+    userId: userId,
+    playerName: playerName,
+    profileCode: profileCode,
+  };
 
-  // api.com/user
-  exports.getUserByPlayername = async function (playerName) {
-    var res = await fetch(`http://localhost:5051/user/playername/${playerName}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  
-    if (res.ok) {
-      return res;
-    } else {
-      return null;
-    }
+  // TODO Refactor and call from apis
+  var res = await fetch(`http://localhost:5051/profile/claim`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  if (res.ok) {
+    return res.json().then((data) => data);
+  } else {
+    return null;
   }
+};
 
-  exports.getUserById = async function(userId) {
-    console.log("calling because data not in cache")
-    var res = await fetch(`http://localhost:5051/user/${userId}`,{
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+exports.unclaimProfile = async function (userId) {
+  const data = { userId: userId };
+  const res = await fetch(`http://localhost:5051/profile/unclaim`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
 
-    if (res.ok) {
-      return res.json().then(data => data);
-    } else {
-      return null;
-    }
+  if (res.ok) {
+    return res.json().then((data) => data);
+  } else {
+    return null;
   }
+};
+
+// api.com/user
+exports.getUserByPlayername = async function (playerName) {
+  var res = await fetch(`http://localhost:5051/user/playername/${playerName}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (res.ok) {
+    return res;
+  } else {
+    return null;
+  }
+};
+
+exports.getUserById = async function (userId) {
+  var res = await fetch(`http://localhost:5051/user/${userId}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (res.ok) {
+    return res.json().then((data) => data);
+  } else {
+    return null;
+  }
+};
