fc466fddca0c942e310c14ef6108c9dc924a5c9b
Albany Patriawan <zekromfx1337@gmail.com>
2023-12-20 19:57:31 -0800

    Custom hook that fetches player name using useQuery (caching + refetching)

---
+++ react/src/hooks/usePlayername.js
@@ -0,0 +1,22 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import {useQuery, useQueryClient, QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
+import {getUserById} from "../apis/apis.js"
+
+// Abstract the implementation of getting the user's playername via custom hook
+// useQuery handles state management for our user data
+export default function usePlayername() {
+    const {user} = useAuth0();
+    const {data, status} =  useQuery({queryKey: ['userData'], queryFn: () => {return getUserById(user?.sub)}})
+
+    if (status === 'pending' || status === 'error') {
+        return "Player name loading...";
+    }
+    
+    return data?.["playerName"];
+}
+
+// todo custom hook that wraps around a useAuth hook?
+
+// * auth0 and useQuery: 2 calls
+// * useQuery: 2 calls
