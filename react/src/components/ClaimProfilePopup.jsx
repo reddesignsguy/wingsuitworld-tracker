55b6df534493e2c64f9845f4be122eb1b48806a6
Albany Patriawan <zekromfx1337@gmail.com>
2023-12-22 10:18:32 -0800

    ClaimProfileModal handles modal switching logic

--- react/src/components/ClaimProfilePopup.jsx
+++ react/src/components/ClaimProfilePopup.jsx
@@ -1,39 +1,66 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import "./Modal.css";
 import AlertBar from "./AlertBar";
+import Modal from "./Modal";
 
 const { useState } = require("react");
 const { claimProfile } = require("../apis/apis");
 
-export default function ClaimProfilePopup(props) {
+// ! PopUp is now decoupled from the App state
+// ! Now we need to find a way to close the popup -> is this managed by a state one level above?
+
+// ! isClosed() is called that way (not close()) to differentiate between local and parent state
+export default function ClaimProfileModal({ isOpen, playerName, onClose }) {
   const { isAuthenticated, user } = useAuth0();
-  const { playerName, setAlertBar } = props; // prop destructuring
   const [input, setInput] = useState("");
+  const title = `Claim ${playerName}'s profile`;
+  const [isClaimSuccessful, setIsClaimSuccessful] = useState(false);
 
   return (
-    <section className="claim-profile-popup">
-      <span>
-        Log in to the game as {playerName} and enter your profile code
-      </span>
-      <input
-        className="profile-code__input"
-        placeholder="123456"
-        onInput={(e) => {
-          // @ts-ignore
-          setInput(e.target.value);
-        }}
-      ></input>
-      <button
-        className="profile-code__input__button"
-        onClick={async () => {
-          setAlertBar(<AlertBar text="You claimed this profile" />);
-          if (isAuthenticated && user) {
-            const userId = user.sub;
-            const claimed = await claimProfile(userId, playerName, input);
-          }
-        }}
-      >
-        Claim
-      </button>
-    </section>
+    <Modal isOpen={isOpen} onClose={onClose}>
+      {/* 1. Claim profile content */}
+      {!isClaimSuccessful && (
+        <section className="claim-profile-popup">
+          <span>
+            Log in to the game as {playerName} and enter your profile code
+          </span>
+          <input
+            className="profile-code__input"
+            placeholder="123456"
+            onInput={(e) => {
+              // @ts-ignore
+              setInput(e.target.value);
+            }}
+          ></input>
+          <button
+            className="profile-code__input__button"
+            onClick={async () => {
+              if (isAuthenticated && user) {
+                const userId = user.sub;
+                const claimed = await claimProfile(userId, playerName, input);
+                if (claimed?.ok) {
+                  setIsClaimSuccessful(true);
+                } else {
+                  setIsClaimSuccessful(true);
+                }
+              }
+            }}
+          >
+            Claim
+          </button>
+        </section>
+      )}
+      {/* 2. Successful claim content */}
+      {isClaimSuccessful && (
+        <>
+          <span> {`You successfuly claimed ${playerName}'s profile.`}</span>
+          <button
+            onClick={() => {
+              onClose();
+            }}
+          ></button>
+        </>
+      )}
+    </Modal>
   );
 }
