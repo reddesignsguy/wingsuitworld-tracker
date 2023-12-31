7c206572789aab67c99d071c3b233d3dfdb32f44
Albany Patriawan <zekromfx1337@gmail.com>
2023-12-31 12:21:26 -0800

    primary button and super primary button separation

--- react/src/components/Buttons/PrimaryButton.jsx
+++ react/src/components/Buttons/PrimaryButton.jsx
@@ -3,48 +3,10 @@ const { Button } = require("./Button");
 
 export const PrimaryButton = styled(Button)`
   background-color: #f08119;
+  border: 2px solid #ff9633;
   color: #050505;
   &:hover {
-    background: linear-gradient(270deg, #d8c70f, #f08119, #d8c70f, #f08119);
+    background: #fc902b;
     background-size: 400% 400%;
-    filter: drop-shadow(0px 00px 5px #f28c26);
-
-    -webkit-animation: AnimationName 2s ease infinite;
-    -moz-animation: AnimationName 2s ease infinite;
-    animation: AnimationName 2s ease infinite;
-  }
-
-  @-webkit-keyframes AnimationName {
-    0% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-    100% {
-      background-position: 0% 50%;
-    }
-  }
-  @-moz-keyframes AnimationName {
-    0% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-    100% {
-      background-position: 0% 50%;
-    }
-  }
-  @keyframes AnimationName {
-    0% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-    100% {
-      background-position: 0% 50%;
-    }
   }
 `;
