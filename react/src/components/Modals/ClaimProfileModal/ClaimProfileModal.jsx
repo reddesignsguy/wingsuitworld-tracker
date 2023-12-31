079debcb7f634680646ff7bae815538b61eaa036
Albany Patriawan <zekromfx1337@gmail.com>
2023-12-31 13:22:58 -0800

    claim profile success modal looks

--- react/src/components/Modals/ClaimProfileModal/ClaimProfileModal.jsx
+++ react/src/components/Modals/ClaimProfileModal/ClaimProfileModal.jsx
@@ -1,12 +1,16 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import "./ClaimProfileModal.css";
 import Modal from "../Modal";
+import { IoCheckmarkCircle } from "react-icons/io5";
 import { SecondaryButton } from "../../Buttons/SecondaryButton";
 import { PrimaryButton } from "../../Buttons/PrimaryButton";
 import { QueryClient, useMutation } from "@tanstack/react-query";
 import { SuperPrimaryButton } from "../../Buttons/SuperPrimaryButton";
+// @ts-ignore
 import { SearchBar } from "../../SearchBar";
 import { TransparentSearchBar } from "../../TransparentSearchBar";
+import styled from "styled-components";
+import { AlternatePrimaryButton } from "../../Buttons/AlternatePrimaryButton";
 
 const { useState } = require("react");
 const { claimProfile } = require("../../../apis/apis");
@@ -20,6 +24,7 @@ export default function ClaimProfileModal({ isOpen, playerName, onClose }) {
   const claimMutation = useMutation({
     mutationFn: async () => {
       if (isAuthenticated && user) {
+        // @ts-ignore
         const result = await claimProfile(user?.sub, playerName, input);
         // TODO: alert onSuccess callback if this is successful (we dont know if returning true is how it's done)
         return true;
@@ -27,6 +32,7 @@ export default function ClaimProfileModal({ isOpen, playerName, onClose }) {
         return false;
       }
     },
+    // @ts-ignore
     onSuccess: (data, variables, context) => {
       console.log(
         "successfully mutated server data, now i should be invalidating the cache"
@@ -45,9 +51,13 @@ export default function ClaimProfileModal({ isOpen, playerName, onClose }) {
           <span className="claim-profile-popup__description">
             1. Log in to the game as <b>{playerName}</b>
             <br />
+            <br />
             2. Click <b>Settings</b>
             <br />
+            <br />
             3. Press <b>Reveal 6-digit profile code</b>
+            <br />
+            <br />
           </span>
           <TransparentSearchBar
             className="profile-code__input"
@@ -62,6 +72,7 @@ export default function ClaimProfileModal({ isOpen, playerName, onClose }) {
             <SecondaryButton onClick={() => onClose()}>Cancel</SecondaryButton>
             <SuperPrimaryButton
               onClick={async () => {
+                // @ts-ignore
                 const res = await claimMutation.mutateAsync();
               }}
             >
@@ -73,14 +84,19 @@ export default function ClaimProfileModal({ isOpen, playerName, onClose }) {
       {/* 2. Successful claim content */}
       {isClaimSuccessful && (
         <>
-          <span> {`You successfuly claimed ${playerName}'s profile.`}</span>
-          <PrimaryButton
+          <IoCheckmarkCircle color="#88d332" size={"150px"} />
+          <h2 className="claim-profile_success__title">Success!</h2>
+          <span>
+            You successfuly claimed <b>{playerName}'s</b> profile.
+          </span>
+          <br />
+          <AlternatePrimaryButton
             onClick={() => {
               onClose();
             }}
           >
             Continue
-          </PrimaryButton>
+          </AlternatePrimaryButton>
         </>
       )}
     </Modal>
