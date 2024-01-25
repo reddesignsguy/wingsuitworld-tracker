15f54392cd8dd08efdc1be56bb3b5aa8939b3a29
Albany Patriawan <zekromfx1337@gmail.com>
2024-01-24 18:11:59 -0800

    Home page redesign

--- react/src/pages/Home/Home.jsx
+++ react/src/pages/Home/Home.jsx
@@ -6,13 +6,14 @@ import { useAuth0 } from "@auth0/auth0-react";
 import background_video from "../../media/videos/background_video.mp4";
 import "./Home.css";
 import { PlayerSearchBar } from "../../components/PlayerSearchBar";
+import { PrimaryButton } from "../../components/Buttons/PrimaryButton";
+import styled from "styled-components";
+
 
 export default function Home() {
   // Can only use react hooks within components/classes
   const [input, setInput] = useState("");
   const [autocomplete, setAutocomplete] = useState([]);
-  const { isAuthenticated, user } = useAuth0();
-  const navigate = useNavigate();
 
   useEffect(() => {
     // TODO: 1) API call to datastore when input changes
@@ -31,22 +32,9 @@ export default function Home() {
           </h2>
           <section className="search_container">
             <PlayerSearchBar
-              placeholder={"Enter a roblox name, eg: builderman"}
-              onInput={(e) => {
-                // @ts-ignore
-                setInput(e.target.value);
-              }}
-              // User searches with ENTER key
-              onKeyUp={(e) => {
-                if (e.key == "Enter") {
-                  navigate(`/player/${input}`);
-                }
-              }}
-            ></PlayerSearchBar>
-            {/* User clicks search button */}
-            <NavLink to={`/player?${input}`}>
-              <IoSearchSharp className="search_button"></IoSearchSharp>
-            </NavLink>
+            input={input}
+            setInput={setInput}
+            />
           </section>
         </section>
         <video className="video" autoPlay loop muted>
